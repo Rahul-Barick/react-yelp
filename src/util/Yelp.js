25a56dcbfca21d1c2df559e52e9285f73e979b17
Rahul Barick <libra2rahul@gmail.com>
2020-06-24 21:57:49 +0530

    Update Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,6 @@ const corsUrl = 'https://cors-anywhere.herokuapp.com/';
 
 const Yelp = {
   search: async (term, location, sortBy) => {
-    console.log(`Enviornment Variable::: ${process.env.API_KEY}, ${process.env.NODE_ENV}`);
     const response = await fetch(
       `${corsUrl}https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {
